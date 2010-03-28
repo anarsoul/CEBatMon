d93e2f216d3f102368aceebab55f97bee74e0fb9
Vasily Khoruzhick <anarsoul@gmail.com>
2010-03-29 00:22:46 +0300

    Add README and COPYING, add copyright header to files
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+/*  CEBatMon - Battery monitor for WinCE with logging capability
+    Copyright (C) 2010 Vasily Khoruzhick (anarsoul@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include <windows.h>
 #include <string.h>
 #include "log.h"
@@ -35,8 +53,9 @@ static void doLogLine()
 		current = stat2.BatteryCurrent;
 		temperature = stat2.BatteryTemperature;
 		acStatus = stat2.ACLineStatus;
-		doLog("; U    |  I    |  T    | %s\n",
+		doLog("; uptime: %.7d, %s\n", uptime,
 			acStatus ? "AC Online" : "AC Offline");
+		doLog("; U    |  I    |  T\n");
 		doLog("%7.d,%7.d,%7.d\n",
 				voltage,
 				current,
